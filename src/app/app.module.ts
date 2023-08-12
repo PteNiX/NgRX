eaa2e48aa797cf0dc15b38802331b4c1766d16ed
Aliaksandr Ptsitsa <ptenix@gmail.com>
2023-08-13 02:06:36 +0300

    init: create component3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Component1Component } from './components/component1/component1.component';
 import { Component2Component } from './components/component2/component2.component';
+import { Component3Component } from './components/component3/component3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Component1Component,
-    Component2Component
+    Component2Component,
+    Component3Component
   ],
   imports: [
     BrowserModule,
