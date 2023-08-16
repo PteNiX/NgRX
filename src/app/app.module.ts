34f5904a8a75ac6a1f0f3cfe291e2443150a054e
Aliaksandr Ptsitsa <ptenix@gmail.com>
2023-08-17 00:31:21 +0300

    refactor: remove spaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Component1Component } from './components/component1/component1.component';
